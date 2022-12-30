cc7a68a07925b42c7f1fffb6440b707d7a84300a
dej000 <dondejavul@yahoo.com>
2022-12-30 16:53:38 +0100

    ok

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import LoginView from '../views/LoginView.vue'
 
+const LoginView= () =>  import(/* webpackChunkName: "group-user" */ '../views/LoginView.vue')
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -25,7 +25,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-     component: () => import(/* webpackChunkName: "group-user" */ '../views/LoginView.vue')
+     component: LoginView
     },
     {
     path: '/signup',
