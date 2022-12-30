16fa7443cfffd4fb3f47e447eafb3bd6d2d99fbc
dej000 <dondejavul@yahoo.com>
2022-12-30 16:33:46 +0100

    ok

--- src/router/index.js
+++ src/router/index.js
@@ -23,7 +23,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/LoginView.vue')
+      component: () => import(/* webpackChunkName: "Login" */ '../views/LoginView.vue')
     },
     {
     path: '/signup',
